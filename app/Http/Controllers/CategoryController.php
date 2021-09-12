4b4731e489af6658af3e98f7faa139c59af7138c
YingJhen-Su <clearsky0330@gmail.com>
2021-09-12 08:34:50 +0000

    [mod] 增加category & tag 新聞列表show function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
@@ -12,8 +13,24 @@ class CategoryController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show(Request $request, $id)
   {
-    //
+    $newsCount = Category::find($id)->news()->count();
+    $newsPerPage = 5;
+    $pageCount = ceil($newsCount / $newsPerPage);
+    $currentPage = isset($request->all()['psge']) ? $request->all()['psge'] : 1;
+
+    $news = Category::find($id)->news()
+                                ->with(['tags'])
+                                ->orderBy('created_at', 'desc')
+                                ->offset($newsPerPage * ($currentPage-1))
+                                ->limit($newsPerPage)
+                                ->get();
+
+    return view('123', [
+      'news' => $news,
+      'newsCount' => $newsCount,
+      'pageCount' => $pageCount
+    ]);
   }
 }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
@@ -12,8 +13,23 @@ class TagController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show(Request $request, $id)
   {
-    //
+    $newsCount = Tag::find($id)->news()->count();
+    $newsPerPage = 5;
+    $pageCount = ceil($newsCount / $newsPerPage);
+    $currentPage = isset($request->all()['psge']) ? $request->all()['psge'] : 1;
+
+    $news = Tag::find($id)->news()
+                          ->orderBy('created_at', 'desc')
+                          ->offset($newsPerPage * ($currentPage-1))
+                          ->limit($newsPerPage)
+                          ->get();
+
+    return view('123', [
+      'news' => $news,
+      'newsCount' => $newsCount,
+      'pageCount' => $pageCount
+    ]);
   }
 }
