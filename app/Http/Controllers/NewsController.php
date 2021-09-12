2792a54c6de21f44e585bf42d7e0e7fc58137e2a
YingJhen-Su <clearsky0330@gmail.com>
2021-09-12 08:35:50 +0000

    [mod] news controller 調整

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\News;
 
 class NewsController extends Controller
 {
@@ -11,9 +12,24 @@ class NewsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+      $newsCount = News::count();
+      $newsPerPage = 5;
+      $pageCount = ceil($newsCount / $newsPerPage);
+      $currentPage = isset($request->all()['psge']) ? $request->all()['psge'] : 1;
+
+      $news = News::orderBy('created_at', 'desc')
+                    ->offset($newsPerPage * ($currentPage-1))
+                    ->limit($newsPerPage)
+                    ->with(['tags'])
+                    ->get();
+
+      return view('123', [
+        'news' => $news,
+        'newsCount' => $newsCount,
+        'pageCount' => $pageCount,
+      ]);
     }
 
     /**
@@ -23,7 +39,7 @@ class NewsController extends Controller
      */
     public function create()
     {
-        //
+      return view('123');
     }
 
     /**
@@ -34,7 +50,15 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validatedData = $this->validateNews($request);
+
+      $news = new News($validatedData);
+      $news->user_id = auth()->id();
+      $news->save();
+
+      $news->tags()->attach($validatedData['tags']);
+
+
     }
 
     /**
@@ -45,7 +69,8 @@ class NewsController extends Controller
      */
     public function show($id)
     {
-        //
+      $news = News::find($id)->get();
+      return view('123', ['news' => $news]);
     }
 
     /**
@@ -56,7 +81,8 @@ class NewsController extends Controller
      */
     public function edit($id)
     {
-        //
+      $news = News::find($id)->get();
+      return view('123', ['news' => $news]);
     }
 
     /**
@@ -68,7 +94,27 @@ class NewsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $news = News::find($id);
+      $tags = $news->tags;
+
+      $validatedData = $this->validateNews($request);
+      $news->update($validatedData);
+
+      $tagChanges = $validatedData['tags'];
+
+      foreach($tagChanges as $tagChange) {
+        if (!in_array($tagChange, $tags)) {
+          $news->tags()->attach($tagChange);
+        }
+      }
+
+      foreach ($tags as $tag) {
+        if (!in_array($tag, $tagChanges)) {
+          $news->tags()->detach($tag);
+        }
+      }
+
+      return view('123');
     }
 
     /**
@@ -79,6 +125,17 @@ class NewsController extends Controller
      */
     public function destroy($id)
     {
-        //
+      News::find($id)->delete();
+      return redirect('123');
+    }
+
+    public function validateNews(Request $request)
+    {
+      return $request->validate([
+        'title' => 'required|string',
+        'content' => 'required|string',
+        'category_id' => 'require|exists:categories,id',
+        'tag'      => 'exists:tags,id'
+      ]);
     }
 }
