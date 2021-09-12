5a6ae23209e0bbadf4d66ec825722e0878132d3c
YingJhen-Su <clearsky0330@gmail.com>
2021-09-12 05:01:55 +0000

    [add] 新增controller架構

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
+}
