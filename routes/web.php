38c2d6c68b242c00ad261c7bc433542338ae162b
YingJhen-Su <clearsky0330@gmail.com>
2021-09-13 15:09:56 +0000

    [mod] route路徑調整 & 修改首頁常數

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
+  
 Route::get('/', 'NewsController@index');
-  Route::get('/news', 'NewsController@index');
+Route::get('/news', 'NewsController@index');
 Route::get('/news/{id}', 'NewsController@show');
 Route::get('/categories/{id}', 'CategoryController@show');
 Route::get('/tags/{id}', 'TagController@show');
 
 Route::prefix('admin')->group(function () {
-  Route::get('/login','Auth\LoginController@showLoginForm');
+  Route::get('/','Auth\LoginController@showLoginForm');
+  Route::get('/login','Auth\LoginController@showLoginForm')->name('login');
   Route::post('/login','Auth\LoginController@login');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::post('/logout','Auth\LoginController@logout');
-  Route::resource('news', 'NewsController');
-  Route::get('/categories/{id}', 'CategoryController@show');
-  Route::get('/tags/{id}', 'TagController@show');
+  Route::resource('news', 'Admin\NewsController');
+  Route::get('/categories/{id}', 'Admin\CategoryController@show');
+  Route::get('/tags/{id}', 'Admin\TagController@show');
 });
-
-
-//Auth::routes();
-//Route::get('/home', 'HomeController@index')->name('home');
